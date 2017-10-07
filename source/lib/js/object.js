da6f55f8641a9a311bdf6afd285d536d7233d73e
Federico Ghedina <fedeghe@gmail.com>
2017-10-07 13:15:36 +0200

    added fromQs method to get params from the url

--- source/lib/js/object.js
+++ source/lib/js/object.js
@@ -14,8 +14,6 @@ $NS$.object = (function (){
         return ret;
     }
 
-    // 
-    // 
     function jCompare(obj1, obj2) {
         // avoid tags
         return  !isNode(obj1)
@@ -134,7 +132,19 @@ $NS$.object = (function (){
      * returning module
      */
     return {
-        
+        fromQs : function () {
+            var els = document.location.search.substr(1).split('&'),
+                i, len, tmp, out = [];
+
+            for (i = 0, len = els.length; i < len; i += 1) {
+                tmp = els[i].split('=');
+
+                // do not override extra path out
+                // 
+                !out[tmp[0]] && (out[tmp[0]] = decodeURIComponent(tmp[1]));
+            }
+            return out;
+        },
 
         clone: function(obj) {
             var self = $NS$.object,
